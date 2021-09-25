a1e94add57e8ffb3d68f42ff93039a2c42745bc5
Iury Souza <iury.souza@klarna.com>
2021-09-25 18:35:08 -0300

    feat(wrapper): add javascript bindings

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
 import { NativeModules } from 'react-native';
 
 type VaccinationtrackerType = {
-  multiply(a: number, b: number): Promise<number>;
+  getVaccinationData(isForced: boolean): Promise<VaccinationData[]>;
 };
 
 const { Vaccinationtracker } = NativeModules;
 
 export default Vaccinationtracker as VaccinationtrackerType;
+
+export type VaccinationData = {
+  total: number;
+  lastUpdate: Date;
+  singleDose: number;
+  fullyVaccinatedPercentage: number;
+  isoCode: string;
+  secondDosePercentage: number;
+  secondDose: number;
+  fullyVaccinated: number;
+  state: string;
+  singleDosePercentage: number;
+  firstDosePercentage: number;
+  firstDose: number;
+};
