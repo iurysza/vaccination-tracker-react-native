bf921f525c8e31cbd927654e13bc21e6c3acc3f4
Iury Souza <iury.souza@klarna.com>
2021-09-25 18:32:17 -0300

    feat(wrapper): implemented error handling and response parsing for android

--- android/src/main/java/com/reactnativevaccinationtracker/VaccinationtrackerModule.kt
+++ android/src/main/java/com/reactnativevaccinationtracker/VaccinationtrackerModule.kt
@@ -1,24 +1,41 @@
 package com.reactnativevaccinationtracker
 
+import android.util.Log
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.github.iurysza.vaccinationtracker.VaccinationTracker
+import com.github.iurysza.vaccinationtracker.cache.DatabaseDriverFactory
+import com.google.gson.Gson
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+
 
 class VaccinationtrackerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "Vaccinationtracker"
-    }
+    private val vaccinationtracker = VaccinationTracker(DatabaseDriverFactory(reactContext))
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
+    override fun getName(): String = "Vaccinationtracker"
+
     @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
+    fun getVaccinationData(isForced: Boolean, promise: Promise) = GlobalScope.launch {
+        runCatching {
+            val response = vaccinationtracker.getVaccinationData(isForced)
+            Log.d(TAG, "response before parsing: $response")
+            promise.resolve(
+                JsonConvert.jsonToReact(JSONArray(gson.toJson(response)))
+            )
+        }.onFailure {
+            Log.d(TAG, "Error while requesting data: $it")
+            promise.reject(it)
+        }
     }
 
-    
+    companion object {
+        private val gson = Gson()
+    }
 }
+
+private const val TAG = "NativeModule"
